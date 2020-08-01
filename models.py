43bb86b96f9463d05f0f00258eed94c8c9861952
Alex Zhou <3150103078@zju.edu.cn>
2020-08-01 10:22:32 -0400

    Use new model

--- models.py
+++ models.py
@@ -1,15 +1,10 @@
-import matplotlib.pyplot as plt
-import itertools
-import pickle
-import imageio
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-import pdb
 from spectral_normalization import SpectralNorm
+from math import sqrt
+import random
+import pdb
 
 def normal_init(m, mean, std):
     if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
@@ -24,91 +19,140 @@ def weights_init_normal(m):
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
-##############################
-#           U-NET
-##############################
+def init_linear(linear):
+    init.xavier_normal(linear.weight)
+    linear.bias.data.zero_()
 
-class UNetDown(nn.Module):
-    def __init__(self, in_size, out_size, normalize=True, dropout=0.0):
-        super(UNetDown, self).__init__()
+def init_conv(conv, glu=True):
+    init.kaiming_normal(conv.weight)
+    if conv.bias is not None:
+        conv.bias.data.zero_()
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input / torch.sqrt(torch.mean(input ** 2, dim=1, keepdim=True) + 1e-8)
+
+class EqualLR:
+    def __init__(self, name):
+        self.name = name
+
+    def compute_weight(self, module):
+        weight = getattr(module, self.name + '_orig')
+        fan_in = weight.data.size(1) * weight.data[0][0].numel()
+        return weight * sqrt(2 / fan_in)
+
+    @staticmethod
+    def apply(module, name):
+        fn = EqualLR(name)
+        weight = getattr(module, name)
+        del module._parameters[name]
+        module.register_parameter(name + '_orig', nn.Parameter(weight.data))
+        module.register_forward_pre_hook(fn)
+        return fn
+
+    def __call__(self, module, input):
+        weight = self.compute_weight(module)
+        setattr(module, self.name, weight)
+
+
+def equal_lr(module, name='weight'):
+    EqualLR.apply(module, name)
+    return module
+
+class EqualLinear(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        linear = nn.Linear(in_dim, out_dim)
+        linear.weight.data.normal_()
+        linear.bias.data.zero_()
+        self.linear = equal_lr(linear)
+
+    def forward(self, input):
+        return self.linear(input)
+
+class AdaptiveInstanceNorm(nn.Module):
+    def __init__(self, in_channel, style_dim):
+        super().__init__()
+        self.norm = nn.InstanceNorm2d(in_channel)
+        self.style = EqualLinear(style_dim, in_channel * 2)
+        self.style.linear.bias.data[:in_channel] = 1
+        self.style.linear.bias.data[in_channel:] = 0
+
+    def forward(self, input, style):
+        style = self.style(style).unsqueeze(2).unsqueeze(3)
+        gamma, beta = style.chunk(2, 1)
+        out = self.norm(input)
+        out = gamma * out + beta
+        return out
+
+class ConvDown(nn.Module):
+    def __init__(self, in_size, out_size, normalize=True):
+        super(ConvDown, self).__init__()
         layers = [nn.Conv2d(in_size, out_size, 4, stride=2, padding=1, bias=False)]
         if normalize:
             layers.append(nn.InstanceNorm2d(out_size, affine=True, track_running_stats=True))
         layers.append(nn.LeakyReLU(0.2))
-        if dropout:
-            layers.append(nn.Dropout(dropout))
         self.model = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.model(x)
 
-class UNetUp(nn.Module):
-    def __init__(self, in_size, out_size, dropout=0.0):
-        super(UNetUp, self).__init__()
+class ConvUp(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(ConvUp, self).__init__()
         layers = [  nn.ConvTranspose2d(in_size, out_size, 4, stride=2, padding=1, bias=False),
                     nn.InstanceNorm2d(out_size, affine=True, track_running_stats=True),
                     nn.ReLU(inplace=True)]
-        if dropout:
-            layers.append(nn.Dropout(dropout))
 
         self.model = nn.Sequential(*layers)
 
-    def forward(self, x, skip_input):
+    def forward(self, x):
         x = self.model(x)
-        x = torch.cat((x, skip_input), 1)
         return x
 
-# G(z)
-class Decoder(nn.Module):
-    # initializers
-    def __init__(self):
-        super(Decoder, self).__init__()
-        self.up1 = UNetUp(100+16, 512)
-        self.up2 = UNetUp(1024, 512)
-        self.up3 = UNetUp(1024, 512)
-        self.up4 = UNetUp(1024, 256)
-        self.up5 = UNetUp(512, 128)
-        self.up6 = UNetUp(256, 64)
-        
-        final = [   nn.ConvTranspose2d(128, 3, 4, stride=2, padding=1),
-                    nn.Tanh() ]
-        self.final = nn.Sequential(*final) 
+class AdaConvUp(nn.Module):
+    def __init__(self, in_size, out_size, style_dim=512):
+        super(AdaConvUp, self).__init__()
+        self.upconv =  nn.ConvTranspose2d(in_size, out_size, 4, stride=2, padding=1, bias=False)
+        self.adain = AdaptiveInstanceNorm(out_size, style_dim)
+        self.relu = nn.ReLU(inplace=True)
 
-    # weight_init
-    def weight_init(self, mean, std):
-        for m in self._modules:
-            normal_init(self._modules[m], mean, std)
+    def forward(self, x, style):
+        x = self.upconv(x)
+        x = self.adain(x, style)
+        x = self.relu(x)
+        return x
 
-    # forward method
-    def forward(self, z, feats):
-        [d1, d2, d3, d4, d5, d6] = feats
-        u1 = self.up1(z, d6)
-        u2 = self.up2(u1, d5)
-        u3 = self.up3(u2, d4)
-        u4 = self.up4(u3, d3)
-        u5 = self.up5(u4, d2)
-        u6 = self.up6(u5, d1)
-        return self.final(u6)
+class toRGB(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(toRGB, self).__init__()
+        self.conv =  nn.Conv2d(in_size, out_size, 3, 1, 1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.tanh(x)
+        return x
 
 class Encoder(nn.Module):
-    def __init__(self, d=16):
+    def __init__(self):
         super(Encoder, self).__init__()
-        self.down1 = UNetDown(3, 64, normalize=False)
-        self.down2 = UNetDown(64, 128)
-        self.down3 = UNetDown(128, 256)
-        self.down4 = UNetDown(256, 512)
-        self.down5 = UNetDown(512, 512)
-        self.down6 = UNetDown(512, 512)
-        self.down7 = UNetDown(512, 512, normalize=False)
+        self.down1 = ConvDown(3, 64, normalize=False) # 64
+        self.down2 = ConvDown(64, 128) # 32
+        self.down3 = ConvDown(128, 256) # 16
+        self.down4 = ConvDown(256, 512) # 8
+        self.down5 = ConvDown(512, 512) # 4
+        self.down6 = ConvDown(512, 512) # 2
+        self.down7 = ConvDown(512, 128, normalize=False) # 1
         
-        self.fc = nn.Linear(512,100)
-
-    # weight_init
     def weight_init(self, mean, std):
         for m in self._modules:
             normal_init(self._modules[m], mean, std)
 
-    # forward method
     def forward(self, x):
         d1 = self.down1(x)
         d2 = self.down2(d1)
@@ -117,20 +161,152 @@ class Encoder(nn.Module):
         d5 = self.down5(d4)
         d6 = self.down6(d5)
         d7 = self.down7(d6)
-        d7 = d7.view(d7.shape[0], -1) 
-        return self.fc(d7), [d1,d2,d3,d4,d5,d6]
+        return d7
 
-    def sample(self, mu, logvar, noise):
-        std = torch.exp(0.5*logvar)
-        return noise.mul(std).add_(mu)
 
-    def kl_div(self, mu, logvar):
-        return -0.5*torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-class discriminator(nn.Module):
-    # initializers
-    def __init__(self, d=128):
-        super(discriminator, self).__init__()
+class DisentangleDecoder(nn.Module):
+    def __init__(self, code_dim=512, n_mlp=8):
+        super(DisentangleDecoder, self).__init__()
+        self.up1 = ConvUp(128, 256) # 2
+        self.up2 = AdaConvUp(256, 512) # 4 <- inject random variable
+        self.up3 = AdaConvUp(512, 512) # 8 <- inject random variable
+        self.up4 = AdaConvUp(512, 512) # 16 <- inject random variable
+        self.up5 = AdaConvUp(512, 512) # 32 <- inject random variable
+        self.up6 = AdaConvUp(512, 256) # 64 <- inject random variable
+        self.up7 = AdaConvUp(256, 128) # 128 <- inject random variable
+
+        self.toRGB_4 = toRGB(512, 3) # 4
+        self.toRGB_8 = toRGB(512, 3) # 8        
+        self.toRGB_16 = toRGB(512, 3) # 16
+        self.toRGB_32 = toRGB(512, 3) # 32
+        self.toRGB_64 = toRGB(256, 3) # 64
+        self.toRGB_128 = toRGB(128, 3) # 128
+        
+        layers = [PixelNorm()]
+        for i in range(n_mlp):
+            layers.append(EqualLinear(code_dim, code_dim))
+            layers.append(nn.LeakyReLU(0.2))
+        self.style_layers = nn.Sequential(*layers)
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, z, style_codes):
+        u1 = self.up1(z)
+        style = self.style_layers(style_codes)
+        u2 = self.up2(u1, style); rgb_4 = self.toRGB_4(u2) # 4
+        u3 = self.up3(u2, style); rgb_8 = self.toRGB_8(u3) # 8
+        u4 = self.up4(u3, style); rgb_16 = self.toRGB_16(u4) # 16
+        u5 = self.up5(u4, style); rgb_32 = self.toRGB_32(u5) # 32
+        u6 = self.up6(u5, style); rgb_64 = self.toRGB_64(u6) # 64
+        u7 = self.up7(u6, style); rgb_128 = self.toRGB_128(u7) # 128   
+        rgb_list = [rgb_4, rgb_8, rgb_16, rgb_32, rgb_64, rgb_128]
+        return rgb_list
+
+class Disc_4(nn.Module):
+    def __init__(self):
+        super(Disc_4, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
+        self.conv2 = SpectralNorm(nn.Conv2d(64, 128, 3, 1, 1))
+        self.conv3 = SpectralNorm(nn.Conv2d(128, 1, 2, 1, 0))
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        z = self.conv3(x)
+        return z
+
+class Disc_8(nn.Module):
+    def __init__(self):
+        super(Disc_8, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
+        self.conv2 = SpectralNorm(nn.Conv2d(64, 128, 4, 2, 1))
+        self.conv3 = SpectralNorm(nn.Conv2d(128, 258, 3, 1, 1))
+        self.conv4 = SpectralNorm(nn.Conv2d(258, 1, 2, 1, 0))
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        z = self.conv4(x)
+        return z
+
+class Disc_16(nn.Module):
+    def __init__(self):
+        super(Disc_16, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
+        self.conv2 = SpectralNorm(nn.Conv2d(64, 256, 4, 2, 1))
+        self.conv3 = SpectralNorm(nn.Conv2d(256, 512, 4, 2, 1))
+        self.conv4 = SpectralNorm(nn.Conv2d(512, 1, 2, 1, 0))
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        z = self.conv4(x)
+        return z
+
+class Disc_32(nn.Module):
+    def __init__(self):
+        super(Disc_32, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
+        self.conv2 = SpectralNorm(nn.Conv2d(64, 128, 4, 2, 1))
+        self.conv3 = SpectralNorm(nn.Conv2d(128, 256, 4, 2, 1))
+        self.conv4 = SpectralNorm(nn.Conv2d(256, 512, 4, 2, 1))
+        self.conv5 = SpectralNorm(nn.Conv2d(512, 1, 2, 1, 0))
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        x = F.leaky_relu(self.conv4(x), 0.2)
+        z = self.conv5(x)
+        return z
+
+class Disc_64(nn.Module):
+    def __init__(self):
+        super(Disc_64, self).__init__()
+        self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
+        self.conv2 = SpectralNorm(nn.Conv2d(64, 128, 4, 2, 1))
+        self.conv3 = SpectralNorm(nn.Conv2d(128, 256, 4, 2, 1))
+        self.conv4 = SpectralNorm(nn.Conv2d(256, 512, 4, 2, 1))
+        self.conv5 = SpectralNorm(nn.Conv2d(512, 1024, 4, 2, 1))
+        self.conv6 = SpectralNorm(nn.Conv2d(1024, 1, 2, 1, 0))
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    def forward(self, input):
+        x = F.leaky_relu(self.conv1(input), 0.2)
+        x = F.leaky_relu(self.conv2(x), 0.2)
+        x = F.leaky_relu(self.conv3(x), 0.2)
+        x = F.leaky_relu(self.conv4(x), 0.2)
+        x = F.leaky_relu(self.conv5(x), 0.2)
+        z = self.conv6(x)
+        return z
+
+class Disc_128(nn.Module):
+    def __init__(self):
+        super(Disc_128, self).__init__()
         self.conv1 = SpectralNorm(nn.Conv2d(3, 64, 4, 2, 1))
         self.conv2 = SpectralNorm(nn.Conv2d(64, 128, 4, 2, 1))
         self.conv3 = SpectralNorm(nn.Conv2d(128, 256, 4, 2, 1))
@@ -139,44 +315,59 @@ class discriminator(nn.Module):
         self.conv6 = SpectralNorm(nn.Conv2d(1024, 1024, 4, 1, 0))
         self.conv7 = SpectralNorm(nn.Conv2d(1024, 1, 1, 1, 0))
 
-    # weight_init
     def weight_init(self, mean, std):
         for m in self._modules:
             normal_init(self._modules[m], mean, std)
 
-    # forward method
     def forward(self, input):
-        # print('input: ', input.shape)
         x = F.leaky_relu(self.conv1(input), 0.2)
-        # print('1: ', x.shape)
         x = F.leaky_relu(self.conv2(x), 0.2)
-        # print('2: ', x.shape)
         x = F.leaky_relu(self.conv3(x), 0.2)
-        # print('3: ', x.shape)
         x = F.leaky_relu(self.conv4(x), 0.2)
-        # print('4: ', x.shape)
         x = F.leaky_relu(self.conv5(x), 0.2)
-        # print('5: ', x.shape)
         x = F.leaky_relu(self.conv6(x), 0.2)
-        # print('6: ', x.shape)
         z = self.conv7(x)
-        # print('7: ', x.shape)
-        # pdb.set_trace()
         return z
 
+def denorm(tensor):
+	return ((tensor+1.0)/2.0)*255.0
 
-
+def norm(image):
+	return (image/255.0-0.5)*2.0
 
 if __name__ == '__main__':
-    x = torch.ones((1,3,128,128))
-    vencoder, decoder, discriminator = VEncoder(), Decoder(), discriminator()
-    mu, logvar, feats = vencoder(x)
-
-    dyna_noise = torch.randn(mu.size())
-    dyna_code = vencoder.sample(mu, logvar, dyna_noise).unsqueeze(2).unsqueeze(3)
-    high_hat = decoder(dyna_code,feats)
-    z = discriminator(high_hat)
+        
+    num_sample = 2 # number of faces you want to sample
+    
+    rgb = torch.ones((1,3,128,128))
+    mask = torch.ones((1,3,128,128))
+    
+    # process inptus
+    mask[mask>0] = 1
+    cond_input = rgb*mask
+    rgb, cond_input = norm(rgb), norm(cond_input)
+    
+    # load models
+    encoder = torch.load('encoder_29.pt', map_location=lambda storage, loc: storage)
+    decoder = torch.load('decoder_29.pt', map_location=lambda storage, loc: storage)
+    
+    # Sample Style Code 
+    batch = rgb.shape[0]
+    style_codes = torch.FloatTensor(batch, num_sample, 512).uniform_().view(-1,512)
+    
+    # Encode occluded face image 
+    code = encoder(cond_input)
+    	
+    # Decode face image
+    rgb_hat = decoder(code, style_codes)[-1]
+    
     pdb.set_trace()
+
+
+
+
+
+
 
 
 
